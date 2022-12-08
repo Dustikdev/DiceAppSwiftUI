1912be0fedb34eec88c29cde3f1db670ee5b6769
Никита Швец <dustikappple@icloud.com>
2022-12-08 12:49:13 +0100

    Initial Commit

---
+++ DiceAppSwiftUI/DiceAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppSwiftUIApp.swift
+//  DiceAppSwiftUI
+//
+//  Created by Никита Швец on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
